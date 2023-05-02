e2c91f4143973460d5c72dcf40b8b97ce2fac48d
jofrantoba <chescot2302@gmail.com>
2023-05-02 10:51:34 -0500

    the first commit devolpay

---
+++ service/src/main/java/com/develtrex/devolpay/service/Service.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.develtrex.devolpay.service;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Service {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
